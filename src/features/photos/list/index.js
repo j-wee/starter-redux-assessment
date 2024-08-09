1fcbfce61288da6427743107b151948223bd69f9
Joan Wee <jwee@indeed.com>
2024-08-09 16:40:23 +0900

    Complete search and filter photo function

--- src/features/photos/list/index.js
+++ src/features/photos/list/index.js
@@ -4,12 +4,13 @@ import {
   removePhoto,
   selectAllPhotos,
   // Task 13: Import the `selectFilteredPhotos()` selector from the photos slice
+  selectFilteredPhotos,
 } from '../photos.slice';
 import './list.css';
 
 export default function PhotosList() {
   // Task 14: Call `useSelector()` below with `selectFilteredPhotos` instead of `selectAllPhotos`
-  const photos = useSelector(selectAllPhotos);
+  const photos = useSelector(selectFilteredPhotos);
   // Task 8: Store a reference to the Redux store's dispatch method in a variable called `dispatch`
   const dispatch = useDispatch();
   function handleDeleteButtonClick(id) {
